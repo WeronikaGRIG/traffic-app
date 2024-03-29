0ed74f4d4d690352f19f8303cf67dd9e2c834e35
WeronikaGRIG <veronikaudot@gmail.com>
2024-03-29 11:00:26 +0100

    add components, delete folder build, new folders structure

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,12 @@
 
 import styles from './Card.module.css';
-import { cards } from './data.js'
-import CardList from './CardList';
+import { cards } from '../../data.js'
+import CardList from '../CardList/CardList';
 
 export default function Card() {
     return (
 
         <div className={styles.cards}>
-
             {cards.map((card, i) => {
                 return (
                     <CardList
@@ -15,8 +14,6 @@ export default function Card() {
                     />
                 )
             })}
-
-
         </div>
 
     )
